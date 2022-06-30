f7c7512776f2019142dad5f5429290ace5321bca
Cihsan <mutamuty@gmail.com>
2022-06-30 15:56:43 +0600

    footer section added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from '../src/Component/Footer'
+//import Home from './Pages/Home';
+function App() {
+  return (
+    <div className="App">
+      <Footer></Footer>
+     {/*  <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
