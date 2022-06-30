4abbacd621dabccbbc0828eaf54d48286c28b981
Cihsan <mutamuty@gmail.com>
2022-06-30 16:28:38 +0600

    added all routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Footer from '../src/Component/Footer'
-//import Home from './Pages/Home';
+import Header from './Component/Header';
+import Home from './Pages/Home';
+import Calendar from './Pages/Calendar';
+import CompleteTask from './Pages/CompleteTask';
+import ToDo from './Pages/ToDo';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <Footer></Footer>
-     {/*  <Routes>
+    <div className="">
+      <Header></Header>
+      <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/home' element={<Home/>}/>
-      </Routes> */}
+        <Route path='/to-do' element={<ToDo/>}/>
+        <Route path='/completed-task' element={<CompleteTask/>}/>
+        <Route path='/calendar' element={<Calendar/>}/>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
