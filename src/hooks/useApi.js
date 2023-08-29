7fee8279f62d1efa267d6e645e807b4377fd1d47
Chy Ihsan <43118152+Cihsan@users.noreply.github.com>
2023-08-29 12:48:14 +0600

    Update useApi

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const useApi = () => {
     const [todos, setTodos] = useState([])
     useEffect(() => {
-        const url = `https://whispering-wildwood-58003.herokuapp.com/`
+        const url = `https://task-management-ixa4.onrender.com//`
         fetch(url)
             .then(res => res.json())
             .then(data => setTodos(data))
